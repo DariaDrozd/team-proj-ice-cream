2560b17381657b6938942bc1a94f469a9e44ba23
DarleneZaremba <darlenezaremba11@gmail.com>
2022-12-25 19:38:58 +0100

    Modal chocolate is ready

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
-
+document.addEventListener('DOMContentLoaded', function () {
+  const modals = ['data-modal'];
+  modals.forEach(element => {
+    const openModalSelector = element + '-open';
+    const closeModalSelector = element + '-close';
+    const openModalBtns = document.querySelectorAll(
+      '[' + openModalSelector + ']'
+    );
+    const closeModalBtns = document.querySelectorAll(
+      '[' + closeModalSelector + ']'
+    );
+    const modal = document.querySelector('[' + element + ']');
+    openModalBtns.forEach(openBtn =>
+      openBtn.addEventListener('click', toggleModal)
+    );
+    closeModalBtns.forEach(closeBtn =>
+      closeBtn.addEventListener('click', toggleModal)
+    );
+    function toggleModal() {
+      document.body.classList.toggle('modal-open');
+      modal.classList.toggle('is-hidden');
+    }
+  });
+});
 
