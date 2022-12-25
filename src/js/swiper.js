e164c48370261b27c600b585ef38c96731cb3706
Andrii Kharchenko <107865478+HAAndrii@users.noreply.github.com>
2022-12-25 16:39:06 +0200

    add slider

---
+++ src/js/swiper.js
@@ -0,0 +1,53 @@
+  // import Swiper bundle with all modules installed
+  import Swiper from 'swiper/bundle';
+
+  // import styles bundle
+  import 'swiper/css/bundle';
+
+(() => {
+   
+    function swiperSlider() {
+        const heroMain = document.querySelectorAll('[data-slider="response-slider"]');
+        if (heroMain) {
+            heroMain.forEach(slider => {
+                let pagination = slider.querySelector('.swiper-pagination');
+                swiper = new Swiper(slider.querySelector('.swiper'), {
+                    speed: 1500,
+                    centeredSlides: true,
+                    autoplay: {
+                        delay: 3000,
+                        disableOnInteraction: false,
+                    },
+                    slidesPerView: 1,
+                    spaceBetween: 30,
+                    pagination: {
+                        el: pagination,
+                        clickable: true,
+                        renderBullet: function (index, className) {
+                            return '<li class="' + className + '"></li>';
+                        },
+                    },
+                    on: {
+                        transitionStart: function () {
+                            let previousIndex = this.previousIndex;
+                            let previousSlide = slider.getElementsByClassName('swiper-slide')[previousIndex];
+                            if (previousSlide) {
+                                setTimeout(function () {
+                                    previousSlide.classList.remove('is-play');
+                                }, 1000);
+                            }
+                        },
+                        transitionEnd: function () {
+                            let activeIndex = this.activeIndex;
+                            let activeSlide = slider.getElementsByClassName('swiper-slide')[activeIndex];
+                            activeSlide.classList.add('is-play');
+                        },
+                    }
+
+                });
+            });
+        }
+    }
+    window.addEventListener('load', swiperSlider, false);
+})();
+
